d1888fef4a889da8e7b8f7894edb3921f5e40bb1
Bryant Eisenbach <fubuloubu@gmail.com>
2019-01-17 14:29:12 -0500

    (refactor) Added QR Print; Removed central coord

--- new-account.py
+++ new-account.py
@@ -1,19 +1,27 @@
 import json
 import getpass
-import requests
+import pyqrcode
 
 from eth_account import Account
 
 # Create a branch new account with a random private key
 account = Account.create(extra_entropy=getpass.getpass("Please smash the keyboard and press [ENTER] when ready :) "))
+print()
 
-# Save the key to a local keystore file
+# Print the account address to the user
+print("Please only send testnet ETH to this account:", account.address)
+qr = pyqrcode.create(account.address)
+print(qr.terminal(quiet_zone=2))
+
+print("Rinkeby Testnet Faucet:", "https://faucet.rinkeby.io/")
+print()
+
+# Encrypt the key with a good passphrase!
 keystore = account.encrypt(getpass.getpass("Please encrypt your keystore file with a password "))
+print()
+
+# Save the encrypted key to a local keystore file
 with open('.your.keys', 'w') as f:
     f.write(json.dumps(keystore))
 print("Saved keystore to ./.your.keys!")
-
-# Communicate address to leader so they can give you ETH!
-data = {'address': account.address}
-r = requests.post(url=input("What is the IP Address of the server? "), data=data)
-print("SUCCESS!" if r.status_code == 200 else "Something went wrong...")
+print()
