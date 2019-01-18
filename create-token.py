897e3d704fbc78beaa4d43c0089439a3cf19bb52
Bryant Eisenbach <fubuloubu@gmail.com>
2019-01-17 22:25:08 -0500

    (refactor) Removed suggestion to use personal info
    Also using click now!

--- create-token.py
+++ create-token.py
@@ -1,46 +1,56 @@
-import argparse
+import click
 import vyper
 
 from eth_dev import DeveloperAccount
 
 
-# Load developer account
-dev = DeveloperAccount('./.your.keys')
-
-# Ask for some stuff
-ap = argparse.ArgumentParser()
-ap.add_argument('name', type=str,
-        help="Add your name, or something else to describe the token")
-ap.add_argument('symbol', type=str,
-        help="Pick a short identifier, 3-4 letters long (like your initials)")
-ap.add_argument('--decimals', type=int, default=0,
+@click.command()
+@click.option('--name',
+        type=str,
+        prompt=True,
+        help="Something to describe the token")
+@click.option('--symbol',
+        type=str,
+        prompt=True,
+        help="Pick a short identifier, 3-4 letters long (all caps suggested)")
+@click.option('--decimals',
+        type=click.IntRange(0, 18),
+        default=0,
         help="Adjust the number of decimal places to display (Default is zero)")
-ap.add_argument('--initial-supply', type=int, default=int(10**6),
+@click.option('--initial-supply',
+        type=click.IntRange(0, 2**256-1),
+        default=10**6,
         help="Adjust the initial supply (Default is 1m)")
-args = ap.parse_args()
-
-assert 0 <= args.decimals < 18, """
-    Initial Supply must be a postive integer less than 18
+def deploy_token(name, symbol, decimals, initial_supply):
     """
-assert 0 <= args.initial_supply < 2**256, """
-    Initial Supply must be a postive integer less than 2^256-1
+    Deploy and ERC20 token (written in Vyper!) to the test network
+
+    NOTE: Do not add personally identifiable information, as it can not be erased!
     """
 
-# Compile the token
-with open('Token.vy', 'r') as f:
-    token_interface = vyper.compile_code(
-            f.read(),
-            output_formats=['abi', 'bytecode', 'bytecode_runtime'],
-        )
+    # Compile the token
+    with open('Token.vy', 'r') as f:
+        token_interface = vyper.compile_code(
+                f.read(),
+                output_formats=['abi', 'bytecode', 'bytecode_runtime'],
+            )
 
-# Deploy with argument choices
-txn_hash = dev.w3.eth.contract(**token_interface).constructor(
-        args.name.encode('utf-8'),  # Token Name
-        args.symbol.encode('utf-8'),  # Ticker Symbol
-        args.decimals,  # Decimals
-        args.initial_supply,  # Initial Supply
-    ).transact({'from':dev.address})
-print('Deploying token! (https://ropsten.etherscan.io/tx/{})'.format(txn_hash.hex()))
+    # Load developer account
+    dev = DeveloperAccount('./.your.keys')
 
-token_address = dev.w3.eth.waitForTransactionReceipt(txn_hash)['contractAddress']
-print('Token Deployed! (https://ropsten.etherscan.io/token/{})'.format(token_address))
+    # Deploy with argument choices
+    if click.confirm("Do you want to deploy the {0} token ({1})?".format(symbol, name)):
+        print(dev.w3.eth.blockNumber)
+        txn_hash = dev.w3.eth.contract(**token_interface).constructor(
+                name.encode('utf-8'),
+                symbol.encode('utf-8'),
+                decimals,
+                initial_supply,
+            ).transact({'from':dev.address})
+        click.echo('Deploying token... (https://ropsten.etherscan.io/tx/{})'.format(txn_hash.hex()))
+
+        token_address = dev.w3.eth.waitForTransactionReceipt(txn_hash)['contractAddress']
+        click.echo('Token Deployed! (https://ropsten.etherscan.io/token/{})'.format(token_address))
+
+if __name__ == '__main__':
+    deploy_token()
